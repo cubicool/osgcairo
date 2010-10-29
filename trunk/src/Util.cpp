be34f5eb73b824c89cbe3e9948a2437b058f9bc9
cubicool <cubicool@17290f4c-1c40-0410-8912-d381ee15664c>
2010-10-29 04:41:45 +0000

    - This SHOULD build now using the cairocks svn external project.   Let me know RIGHT AWAY if it fails somehow.

--- trunk/src/Util.cpp
+++ trunk/src/Util.cpp
@@ -5,39 +5,61 @@
 #include <osg/Notify>
 #include <osgCairo/Util>
 
+#include "cairocks.h"
+
 namespace osgCairo {
 namespace util {
 
-void writeToPNG(cairo_surface_t* surface, const std::string& path) {
+bool writeToPNG(cairo_surface_t* surface, const std::string& path) {
 #ifdef CAIRO_HAS_PNG_FUNCTIONS
 	cairo_surface_write_to_png(surface, path.c_str());
+
+	return true;
 #else
 	osg::notify(osg::WARN) << "Your version of Cairo does not support PNG writing!" << std::endl;
+
+	return false;
 #endif
 }
 
 bool roundedRectangle(
-	cairo_t*            c,
-	double              x,
-	double              y,
-	double              width,
-	double              height,
-	double              radius
-	const cairo_bool_t* corners
+	cairo_t*       c,
+	double         x,
+	double         y,
+	double         width,
+	double         height,
+	double         radius,
+	const Corners& corners
 ) {
-	return cairocks_rounded_rectangle(c, x, y, width, height, radius, corners);
+	return cairocks_rounded_rectangle(c, x, y, width, height, radius, corners.corners);
 }
 
 bool roundedRectangleApply(
-	cairo_t*            c,
-	double              x,
-	double              y,
-	double              width,
-	double              height,
-	double              radius
-	const cairo_bool_t* corners
+	cairo_t*       c,
+	double         x,
+	double         y,
+	double         width,
+	double         height,
+	double         radius,
+	const Corners& corners
 ) {
-	return cairocks_rounded_rectangle_apply(c, x, y, width, height, radius, corners);
+	return cairocks_rounded_rectangle_apply(c, x, y, width, height, radius, corners.corners);
+}
+
+bool gaussianBlur(cairo_surface_t* surface, double radius, double deviation) {
+	return cairocks_gaussian_blur(surface, radius, deviation);
+}
+
+bool mapPathOnto(cairo_t* c, cairo_path_t* path) {
+	return cairocks_map_path_onto(c, path);
+}
+
+cairo_surface_t* createEmbossedSurface(
+	cairo_surface_t* surface,
+	double           azimuth,
+	double           elevation
+) {
+	return cairocks_create_embossed_surface(surface, azimuth, elevation);
 }
 
 std::string cairoFormatAsString(cairo_format_t format) {
