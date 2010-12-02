c26eb88fa39de1babe380650a1ecfd35b923801a
cubicool <cubicool@17290f4c-1c40-0410-8912-d381ee15664c>
2010-12-02 07:31:20 +0000

    Updated comments in FindWin32GTKBundle.cmake

--- trunk/etc/FindWin32GTKBundle.cmake
+++ trunk/etc/FindWin32GTKBundle.cmake
@@ -1,12 +1,13 @@
+# This file adds support for the GTK+ Win32 Development bundle. It is REALLY
+# unlikely that anyone would EVER try and build all this by default.
+# You can get the bundle from:
+#
+# http://ftp.gnome.org/pub/gnome/binaries/win32/gtk+/2.22/gtk+-bundle_2.22.0-20101016_win32.zip
+#
+# This bundle was tested against on 12/2/2010 by Jeremy Moles.
+
 SET(WIN32_GTK_BUNDLE_DIR "" CACHE PATH "Location of the GTK+ Win32 bundle.")
 
-FIND_PATH(CAIRO_INCLUDE_DIRS cairo.h ${WIN32_GTK_BUNDLE_DIR}/include/cairo)
-
-IF (WIN32)
-	SET(LIBCAIRO "cairo.lib")
-ELSE (WIN32)
-	SET(LIBCAIRO "libcairo.so")
-ENDIF (WIN32)
-
-FIND_PATH(CAIRO_LIBRARY_DIRS ${LIBCAIRO} ${WIN32_GTK_BUNDLE_DIR}/lib)
+FIND_PATH(CAIRO_INCLUDE_DIR cairo.h ${WIN32_GTK_BUNDLE_DIR}/include/cairo)
+FIND_PATH(CAIRO_LIBRARY_DIR cairo.lib ${WIN32_GTK_BUNDLE_DIR}/lib)
 
