7f26fd8b022f60a9aec22c9605fe3d08683dcc53
cubicool <cubicool@17290f4c-1c40-0410-8912-d381ee15664c>
2011-07-22 19:50:25 +0000

    - Better documentation in headers. - Argument names in headers. - Removed README.

--- trunk/examples/osgcairodistancefield/osgcairodistancefield.cpp
+++ trunk/examples/osgcairodistancefield/osgcairodistancefield.cpp
@@ -2,32 +2,33 @@
 #include <cstdlib>
 #include <osg/ArgumentParser>
 #include <osg/Texture2D>
+#include <osg/TextureRectangle>
+#include <osg/BlendFunc>
 #include <osg/MatrixTransform>
 #include <osgGA/GUIEventHandler>
 #include <osgDB/FileUtils>
 #include <osgViewer/Viewer>
-#include <osgText/Text>
 #include <osgCairo/Image>
 #include <osgCairo/Util>
 
-const unsigned int WIDTH      = 800;
-const unsigned int HEIGHT     = 600;
+/* NOTES!
+ *
+ * View Matrix/Shader Scale Ratio (4096 200 x)
+ * 16 = 1:1
+ * 32 = 2:1
+ * 64 = 4:1
+ *
+ */
+
+const unsigned int WIDTH      = 1024;
+const unsigned int HEIGHT     = 768;
 const float        SCALE_STEP = 0.25f;
 
-osg::Image* createDistanceFieldImage(unsigned int size, int scanSize, int blockSize) {
+cairo_surface_t* createDistanceField(unsigned int size, int scanSize, int blockSize) {
 	cairo_surface_t*      surface = cairo_image_surface_create(CAIRO_FORMAT_A8, size, size);
 	cairo_t*              c       = cairo_create(surface);
-	cairo_font_options_t* fo      = cairo_font_options_create();
 
-	cairo_font_options_set_hint_style(fo, CAIRO_HINT_STYLE_NONE);
-
-	cairo_set_antialias(c, CAIRO_ANTIALIAS_NONE);
-	cairo_set_font_options(c, fo);
-	cairo_scale(c, size, size);
-	cairo_select_font_face(c, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-	cairo_set_font_size(c, 0.8f);
-	cairo_translate(c, 0.25f, 0.8);
-	cairo_text_path(c, "A");
+	cairo_arc(c, size / 2.0f, size / 2.0f, size / 3.0f, 0.0f, 2.0f * osg::PI);
 	cairo_fill(c);
 
 	OSG_NOTICE << "Creating Distance Field... " << std::endl;
@@ -38,40 +39,36 @@ osg::Image* createDistanceFieldImage(unsigned int size, int scanSize, int blockS
 
 	OSG_NOTICE << "Took " << t.elapsedTime() << " seconds." << std::endl;
 
-	// osgCairo::util::writeToPNG(surface, "surface.png");
-	// osgCairo::util::writeToPNG(distanceField, "distanceField.png");
+	osgCairo::util::writeToPNG(surface, "surface.png");
+	osgCairo::util::writeToPNG(distanceField, "distanceField.png");
 
-	// Reset our size.
-	size = cairo_image_surface_get_width(distanceField);
-
-	cairo_font_options_destroy(fo);
 	cairo_surface_destroy(surface);
 	cairo_destroy(c);
 
-	osgCairo::Image* image = new osgCairo::Image();
+	return distanceField;
+}
+
+osg::Geode* createDistanceFieldGeode(cairo_surface_t* distanceField) {
+	osg::Geode*      geode   = new osg::Geode();
+	osgCairo::Image* image   = new osgCairo::Image();
+	osg::Texture2D*  texture = new osg::Texture2D();
+	unsigned int     size    = cairo_image_surface_get_width(distanceField);
 
 	if(image->allocateSurface(size, size, CAIRO_FORMAT_A8)) {
-		c = image->createContext();
+		cairo_t* c = image->createContext();
 
 		if(!cairo_status(c)) {
 			cairo_set_source_surface(c, distanceField, 0, 0);
 			cairo_paint(c);
 			cairo_destroy(c);
-
-			cairo_surface_destroy(distanceField);
 		}
 
 		image->flipVertical();
 		image->dirty();
 	}
 
-	return image;
-}
-
-osg::Geometry* createDistanceFieldGeometry(osg::Image* image) {
-	osg::Texture2D* texture  = new osg::Texture2D();
 	osg::Geometry*  geometry = osg::createTexturedQuadGeometry(
-		osg::Vec3(-(image->s() / 2.0f), -(image->t() / 2.0f), -0.1f),
+		osg::Vec3(-(image->s() / 2.0f), -(image->t() / 2.0f), 0.0f),
 		osg::Vec3(image->s(), 0.0f, 0.0f),
 		osg::Vec3(0.0f, image->t(), 0.0f),
 		0.0f,
@@ -81,21 +78,107 @@ osg::Geometry* createDistanceFieldGeometry(osg::Image* image) {
 	);
 	
 	texture->setImage(image);
-
 	texture->setDataVariance(osg::Object::DYNAMIC);
 
 	osg::StateSet* state = geometry->getOrCreateStateSet();
 
-	state->setTextureAttributeAndModes(
-		0,
-		texture,
-		osg::StateAttribute::ON
-	);
-
+	state->setTextureAttributeAndModes(0, texture, osg::StateAttribute::ON);
 	state->setMode(GL_BLEND, osg::StateAttribute::ON);
 	state->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
 
-	return geometry;
+	geode->addDrawable(geometry);
+
+	return geode;
+}
+
+osg::Geode* createGUIGeode(cairo_surface_t* distanceField, unsigned int guiWidth) {
+	osg::Geode*            geode   = new osg::Geode();
+	osgCairo::Image*       image   = new osgCairo::Image();
+	osg::TextureRectangle* texture = new osg::TextureRectangle();
+	unsigned int           size    = cairo_image_surface_get_width(distanceField);
+
+	if(image->allocateSurface(guiWidth, size + 20, CAIRO_FORMAT_ARGB32)) {
+		cairo_t* c = image->createContext();
+
+		if(!cairo_status(c)) {
+			// Draw the texture and the box around the texture.
+			cairo_save(c);
+			
+			cairo_set_source_rgba(c, 0.2f, 0.2f, 0.2f, 0.8f);
+			cairo_paint(c);
+
+			cairo_set_source_rgba(c, 1.0f, 1.0f, 1.0f, 1.0f);
+			cairo_set_line_width(c, 1.0f);
+			cairo_translate(c, 9.0f, 9.0f);
+			cairo_rectangle(c, 0.5f, 0.5f, size + 2.0f, size + 2.0f);
+			cairo_stroke(c);
+
+			cairo_set_source_surface(c, distanceField, 1, 1);
+			cairo_paint(c);
+
+			cairo_restore(c);
+
+			cairo_font_options_t* fo = cairo_font_options_create();
+
+			cairo_font_options_set_hint_style(fo, CAIRO_HINT_STYLE_FULL);
+
+			float fontSize = 15.0f;
+
+			cairo_set_antialias(c, CAIRO_ANTIALIAS_SUBPIXEL);
+			cairo_set_font_options(c, fo);
+			cairo_select_font_face(c, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+			cairo_set_font_size(c, fontSize);
+			cairo_set_source_rgba(c, 1.0f, 1.0f, 1.0f, 1.0f);
+
+			const char* help[] = {
+				"The generated texture is to the left.",
+				"",
+				"Use the U and I keys to adjust the ViewMatrix scale.",
+				"Use the J and K keys to adjust the Shader scale.",
+				"Use the O and P kess to adjust both scales at once."
+			};
+
+			cairo_translate(c, size + 20.0f, 10.0f);
+
+			for(unsigned int i = 0; i < 5; i++) {
+				cairo_translate(c, 0, fontSize);
+				cairo_text_path(c, help[i]);
+				cairo_fill(c);
+			}
+
+			cairo_font_options_destroy(fo);
+			cairo_destroy(c);
+		}
+
+		image->flipVertical();
+		image->dirty();
+	}
+
+	osg::Geometry*  geometry = osg::createTexturedQuadGeometry(
+		osg::Vec3(0.0f, 0.0f, 0.0f),
+		osg::Vec3(image->s(), 0.0f, 0.0f),
+		osg::Vec3(0.0f, image->t(), 0.0f),
+		0.0f,
+		0.0f, 
+		image->s(),
+		image->t()
+	);
+	
+	texture->setImage(image);
+	texture->setDataVariance(osg::Object::DYNAMIC);
+
+	osg::StateSet* state = geometry->getOrCreateStateSet();
+
+	state->setTextureAttributeAndModes(0, texture, osg::StateAttribute::ON);
+	state->setMode(GL_BLEND, osg::StateAttribute::ON);
+	state->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
+	state->setAttributeAndModes(
+		new osg::BlendFunc(osg::BlendFunc::ONE, osg::BlendFunc::ONE_MINUS_SRC_ALPHA)
+	);
+
+	geode->addDrawable(geometry);
+
+	return geode;
 }
 
 class ScaleSet: public osgGA::GUIEventHandler {
@@ -157,6 +240,9 @@ public:
 		aMin->set(std::max(0.0f, 0.5f - 0.07f / scale));
 		aMax->set(std::min(0.5f + 0.07f / scale, 1.0f));
 
+		OSG_NOTICE << "AlphaMin: " << std::max(0.0f, 0.5f - 0.07f / scale) << std::endl;
+		OSG_NOTICE << "AlphaMax: " << std::min(0.5f + 0.07f / scale, 1.0f) << std::endl;
+
 		return true;
 	}
 
@@ -184,35 +270,9 @@ osg::Camera* createOrthoCamera(float width, float height, int orderNum, bool cen
 	camera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
 	camera->setViewMatrix(osg::Matrix::identity());
 	camera->setClearMask(GL_DEPTH_BUFFER_BIT);
-	camera->setRenderOrder(osg::Camera::POST_RENDER);
+	camera->setRenderOrder(osg::Camera::POST_RENDER, orderNum);
 
 	return camera;
-}
-
-osg::Group* createGUI(osg::Geometry* geometry, osg::Vec3::value_type x, osg::Vec3::value_type y) {
-	osg::MatrixTransform* mt    = new osg::MatrixTransform();
-	osg::Geode*           geode = new osg::Geode();
-	osgText::Text*        text  = new osgText::Text();
-
-	text->setFont(osgText::readFontFile("arial.ttf"));
-	text->setCharacterSize(20.0f);
-	text->setFontResolution(20.0f, 20.0f);
-	text->setPosition(osg::Vec3(x, -y, 0.0f));
-	text->setAlignment(osgText::Text::LEFT_BOTTOM);
-	text->setAxisAlignment(osgText::Text::XY_PLANE);
-	text->setText(
-		"+/-: increase, decrease global scale\n"
-		"[/]: increase, decrease shader scale ONLY\n"
-		"\\: print scale values"
-	);
-
-	geode->addDrawable(geometry);
-	geode->addDrawable(text);
-
-	mt->setMatrix(osg::Matrix::translate(x, y, 0.0f));
-	mt->addChild(geode);
-
-	return mt;
 }
 
 int main(int argc, char** argv) {
@@ -246,24 +306,24 @@ int main(int argc, char** argv) {
 	vertPath = osgDB::findDataFile(vertPath);
 	fragPath = osgDB::findDataFile(fragPath);
 
-	osg::Image* image = createDistanceFieldImage(
+	cairo_surface_t* distanceField = createDistanceField(
 		std::atoi(args[1]),
 		std::atoi(args[2]),
 		std::atoi(args[3])
 	);
-	
-	osg::Geometry* geometry  = createDistanceFieldGeometry(image);
-	osg::Camera*   camera    = createOrthoCamera(WIDTH, HEIGHT, 1, true);
-	osg::Camera*   guiCamera = createOrthoCamera(WIDTH, HEIGHT, 0);
-	osg::Program*  program   = new osg::Program();
-	osg::Shader*   vert      = osg::Shader::readShaderFile(osg::Shader::VERTEX, vertPath);
-	osg::Shader*   frag      = osg::Shader::readShaderFile(osg::Shader::FRAGMENT, fragPath);
+
+	osg::Geode*   geode     = createDistanceFieldGeode(distanceField);
+	osg::Camera*  camera    = createOrthoCamera(WIDTH, HEIGHT, 0, true);
+	osg::Camera*  guiCamera = createOrthoCamera(WIDTH, HEIGHT, 1);
+	osg::Program* program   = new osg::Program();
+	osg::Shader*  vert      = osg::Shader::readShaderFile(osg::Shader::VERTEX, vertPath);
+	osg::Shader*  frag      = osg::Shader::readShaderFile(osg::Shader::FRAGMENT, fragPath);
 
 	float scale  = 1.0f;
 	float aMin   = std::max(0.0f, 0.5f - 0.07f / scale);
 	float aMax   = std::min(0.5f + 0.07f / scale, 1.0f);
-	float radius = 20.0f; //80.0f / 32.0f; // distanceFieldRadius
-	float ol     = std::max(double(0.2f), double(0.5 - 0.5 / radius / 1.0f)); 
+	float radius = 1.0f;
+	float ol     = std::max(double(0.2f), double(0.5 - 0.5 / radius)); 
 	float oaMax0 = std::max(double(0.0f), ol - 0.07 / scale);
 	float oaMax1 = std::min(ol + 0.07 / scale, double(aMin));
 
@@ -288,14 +348,9 @@ int main(int argc, char** argv) {
 	camera->getOrCreateStateSet()->addUniform(alphaMax0);
 	camera->getOrCreateStateSet()->addUniform(alphaMax1);
 	camera->getOrCreateStateSet()->addUniform(texture);
-	
-	osg::Geode* geode = new osg::Geode();
-
-	geode->addDrawable(geometry);
-
 	camera->addChild(geode);
 
-	guiCamera->addChild(createGUI(geometry, image->s() / 2.0f, image->t() / 2.0f));
+	guiCamera->addChild(createGUIGeode(distanceField, WIDTH));
 
 	osg::Group* group = new osg::Group();
 
@@ -306,6 +361,10 @@ int main(int argc, char** argv) {
 	viewer.setUpViewInWindow(50, 50, WIDTH, HEIGHT);
 	viewer.addEventHandler(new ScaleSet(camera));
 
-	return viewer.run();
+	int r = viewer.run();
+
+	cairo_surface_destroy(distanceField);
+
+	return r;
 }
 
